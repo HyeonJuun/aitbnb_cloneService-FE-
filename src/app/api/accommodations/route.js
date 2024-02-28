f8cf70afc50bde4f0c684e3243a239fd1b340f5a
luda412 <201810791@sangmyung.kr>
2024-02-28 20:05:40 +0900

    Feat: 애런님, data 작업

--- src/app/api/accommodations/route.js
+++ src/app/api/accommodations/route.js
@@ -1,37 +1,36 @@
-// // import results from '@/app/api/accommodations.json';
-// import accommodations from '@/dummy/accommodations.json';
+// import results from '@/app/api/accommodations.json';
+import accommodations from '@/dummy/accommodations.json';
 
-// export async function GET(request, response) {
-//   // 검색 API 용
-//   const results = accommodations.results
-//     .filter(() => {
-//       request;
-//     })
-//     .map((eachAccommodation) => ({
-//       name: eachAccommodation.name,
-//       age: eachAccommodation.age,
-//     }));
-//   return Response.json({ results });
-// }
+export async function POST(request, response) {
+  // 검색 API 용
+  const results = accommodations.results
+    .filter(() => {
+      //   request;
+      return true;
+    })
+    .map((eachAccommodation) => ({
+      title: eachAccommodation.title,
+    }));
+  return Response.json({ results });
+}
 
-// export async function GET1(request, response) {
-//   // 단일 정보용
-//   const found = accommodations.results.find(
-//     (eachAccommodation) => eachAccommodation.id === request.param.id,
-//   );
-//   return Response.json({ result: found[0] });
-// }
+export async function GET1(request, response) {
+  // 검색 API 용
+  const results = accommodations.results
+    .filter(() => {
+      request;
+    })
+    .map((eachAccommodation) => ({
+      name: eachAccommodation.name,
+      age: eachAccommodation.age,
+    }));
+  return Response.json({ results });
+}
 
-//json
-// {
-//     "results": [
-//       {
-//         "name": "애런호텔",
-//         "location": "KR"
-//       },
-//       {
-//         "name": "바론호텔",
-//         "location": "EN"
-//       }
-//     ]
-//   }
+export async function GET2(request, response) {
+  // 단일 정보용
+  const found = accommodations.results.find(
+    (eachAccommodation) => eachAccommodation.id === request.param.id,
+  );
+  return Response.json({ result: found[0] });
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
 import ListingRooms from '@/app/components/listings/LilstingRooms';
 
-export default function Home() {
+export default async function Home() {
+  const result = await fetch('http://localhost:3000/api/accommodations', {
+    method: 'POST',
+  });
+  const roomdatas = await result.json();
+  console.log(roomdatas.results.map((each: any) => each.title));
   return (
     <>
       <div className='text-rose-500 text-2xl'>Hello Airbnb!</div>
       <div className='mt-8'>
         <ListingRooms />
       </div>
+      {roomdatas.results.map((each: any, index: number) => (
+        <div key={index}>
+          <div>{each.title}</div>
+          <div>{each.description}</div>
+          <div>{each.price_per_night}</div>
+        </div>
+      ))}
     </>
   );
 }
